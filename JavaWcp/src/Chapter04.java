fc0d2915265e80ab82683e2834ce0dbbf032de23
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-05-13 13:33:12 +0900

    [Finish]Chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "せっきー";
+		final int LUKY_NUMBER = 7;
+		System.out.println(str);
+		str = "ニックネームです";
+		System.out.println(str);
+		System.out.println(LUKY_NUMBER);
+	}
+}
