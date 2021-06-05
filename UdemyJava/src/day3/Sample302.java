656185f966f5b0e0eadc54c6ec946b28ca34e740
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-06-06 01:00:05 +0900

    Udamy-3

---
+++ UdemyJava/src/day3/Sample302.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class Sample302 {
+
+	public static void main(String[] args) {
+		int a = -5;
+		System.out.println("a=" + a);
+		if(a > 0) {
+			System.out.println("aはの数です。");
+		} else {
+			System.out.println("aは正の数ではありません。");
+		}
+		
+		a = 5;
+		System.out.println("a=" + a);
+		if(a > 0) {
+			System.out.println("aはの数です。");
+		} else {
+			System.out.println("aは正の数ではありません。");
+		}
+	}
+
+}
---
+++ UdemyJava/src/day3/Sample303.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class Sample303 {
+
+	public static void main(String[] args) {
+		int num = 3;
+        if(num == 1){
+            System.out.println("one");    //  numが1だった場合の処理
+        }else if(num == 2){
+            System.out.println("two");    //  numが2だった場合の処理
+        }else if(num == 3){
+            System.out.println("three");  //  numが3だった場合の処理
+        }else{
+            System.out.println("不適切な値です。"); //  それ以外の値が入力された場合の処理
+        }
+
+	}
+
+}
